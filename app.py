b1436175917177e8a45c7641d758f8803e936146
boraminfo <boraminfo@gmail.com>
2025-06-01 17:18:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,6 @@ from flask import render_template
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)  # ✅ Render에서 감지 가능한 포트
 
 
